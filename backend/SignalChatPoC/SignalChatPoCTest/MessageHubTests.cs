b4ed9197bcb9ab7cb638aab39550435daf05415c
KasiaK <kasiakadziolka@gmail.com>
2026-05-07 08:40:16 +0200

    Init unit tests for hub

---
+++ backend/SignalChatPoC/SignalChatPoCTest/MessageHubTests.cs
@@ -0,0 +1,15 @@
+﻿namespace SignalChatPoCTest;
+
+public class MessageHubTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
